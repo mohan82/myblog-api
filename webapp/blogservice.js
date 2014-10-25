55757dffc628107aefe7f8b2c705da1de458dd60
Mohan Ambalavanan <mohan.ambalavanan@gmail.com>
2014-10-25 19:05:01 +1100

    Removed unnecessary files

--- webapp/blogservice.js
+++ webapp/blogservice.js
@@ -1,9 +1,8 @@
 "use strict"
 var BlogParser = require("blogparser");
 var TEST_FILE = "test.xml";
-function helloWorld(){
-    return "hello";
-}var BlogService = function () {
+
+var BlogService = function () {
     this.blogParser = new BlogParser(TEST_FILE);
 };
 BlogService.prototype.getComments = function () {
