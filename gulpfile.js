a7307a983846c6fbde3a63ff3728c21e842e103f
Mohan Ambalavanan <mohan.ambalavanan@gmail.com>
2015-02-12 14:19:16 +1100

    Added jscs   as gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var jshint = require('gulp-jshint');
 var istanbul = require('gulp-istanbul');
 var jshintStylish = require('jshint-stylish');
 var mocha = require('gulp-mocha');
+var jscs = require('gulp-jscs');
 
 require('shelljs/global');
 
@@ -12,7 +13,10 @@ var BLOG_PARSER_DIR = "blogparser/";
 var WEBAPP_DIR = "webapp/";
 var PERSISTENCE_DIR = "persistence/";
 var TEST_DIR = "*/test/*.js";
-
+var BUILD_DIR = "build/"
+//ignore test,build and node modules dir
+var CODE_DIR = ["!**/**/test/*.js",
+    "!**/node_modules/**/*.js", "**/**/*.js", "!**/build/**/"];
 
 function executeShellCommand(command) {
     exec(command, {silent: false});
@@ -30,11 +34,17 @@ gulp.task("default", ["build"]);
 gulp.task("test", function () {
     executeShellCommand("mocha -R spec " + TEST_DIR);
 });
-gulp.task("clean-all", ["clean-common", "clean-blogparser", "clean-persistence", "clean-webapp"]);
+gulp.task("clean-all", ["clean-build","clean-common", "clean-blogparser", "clean-persistence", "clean-webapp"]);
 gulp.task("build-all", ["build-common", "build-blogparser", "build-persistence", "build-webapp"]);
 
 
 gulp.task("clean-persistence", function () {
+    console.log("Cleaning up Persistence module");
+    executeShellCommand("rm -r "+BUILD_DIR);
+
+});
+
+gulp.task("clean-build",function(){
     console.log("Cleaning up Persistence module");
     executeShellCommand("rm -r " + PERSISTENCE_DIR + "node_modules");
 
@@ -86,29 +96,32 @@ gulp.task('execute-test', function () {
 
 //Linting  and Code Quality Checks
 gulp.task('jshint', function () {
-    //ignore test and node modules dir for code quality
-    var codeQualityDir = ["!**/**/test/*.js",
-        "!**/node_modules/**/*.js", "**/**/*.js", "!**/build/**/"];
 
-    return gulp.src(codeQualityDir).
+
+    return gulp.src(CODE_DIR).
         pipe(jshint()).
         pipe(jshint.reporter(jshintStylish)).
         pipe(jshint.reporter('fail'));
 
 });
 
+gulp.task('jscs', function () {
+
+    return gulp.src(CODE_DIR).
+        pipe(jscs());
+});
+
 gulp.task('coverage', function () {
-    var coverageDir = ["**/**/*.js", "!**/node_modules/**/*.js", "!**/build/**/"];
-    return gulp.src(coverageDir)
+    return gulp.src(CODE_DIR)
         .pipe(istanbul({includeUntested: true})) // Covering files
         .pipe(istanbul.hookRequire()) // Force `require` to return covered files
         .on('finish', function () {
-            gulp.src(['*/test/*.js'])
+            gulp.src(TEST_DIR)
                 .pipe(mocha({reporter: 'spec'}))
                 .pipe(istanbul.writeReports({
-                    dir: './build/unit-test-coverage',
+                    dir: BUILD_DIR +"unit-test-coverage",
                     reporters: ['lcov'],
-                    reportOpts: {dir: './build/unit-test-coverage'}
+                    reportOpts: {dir: BUILD_DIR + "unit-test-coverage"}
                 }));
 
         });
